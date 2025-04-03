00cdd84f5b5d00245e495d782efe43cc0e6fd91b
Rajnish Kumar <rajnishkr.22.11.48@gmail.com>
2025-04-03 12:19:57 +0530

    Update db_connect.php

--- config/db_connect.php
+++ config/db_connect.php
@@ -5,11 +5,13 @@ $password = "";  // No password by default
 $dbname_client = "client_database";
 $dbname_company = "company_database";
 $dbname_auth = "auth_database";
+$dbname_ads = "client_data";
 
 // Create connections
 $client_conn = new mysqli($servername, $username, $password, $dbname_client);
 $company_conn = new mysqli($servername, $username, $password, $dbname_company);
 $auth_conn = new mysqli($servername, $username, $password, $dbname_auth);
+$ads_conn = new mysqli($servername , $username , $password , $dbname_ads);
 
 // Check connections
 if ($client_conn->connect_error || $company_conn->connect_error || $auth_conn->connect_error) {
